5c8db5cc83a7109c6f28b56b05d3b261b5c7af5f
Mariah Legg <mariah.legg@gmail.com>
2022-01-07 14:24:27 -0500

    Removed old, commented out codefor playGame.

--- src/main/java/com/game/gameapp/controller/PlayerController.java
+++ src/main/java/com/game/gameapp/controller/PlayerController.java
@@ -5,6 +5,8 @@ import com.game.gameapp.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
@@ -30,7 +32,7 @@ public class PlayerController {
     // http://localhost:9092/api/player/{playerId}
     @GetMapping(path = "/player/{playerId}")
     public Optional<Player> getPlayer(@PathVariable Long playerId) {
-        LOGGER.info("Calling getPlayer method from controller.");
+        LOGGER.info("Calling getPlayer method from player controller.");
         return playerService.getPlayer(playerId);
     }
 
@@ -38,35 +40,35 @@ public class PlayerController {
     // http://localhost:9092/api/player
     @PostMapping(path = "/player")
     public Player createPlayer(@RequestBody Player playerObject) {
-        LOGGER.info("Calling createPlayer method from controller.");
+        LOGGER.info("Calling createPlayer method from player controller.");
         return playerService.createPlayer(playerObject);
     }
 
     // http://localhost:9092/api/player/{playerId}
     @PutMapping(path ="/player/{playerId}")
     public Player updatePlayer(@PathVariable Long playerId, @RequestBody Player playerObject) {
-        LOGGER.info("Calling updatePlayer method from controller.");
+        LOGGER.info("Calling updatePlayer method from player controller.");
         return playerService.updatePlayer(playerId, playerObject);
     }
 
     // http://localhost:9092/api/player/{playerId}
     @DeleteMapping(path ="/player/{playerId}")
     public String deleteCategory(@PathVariable Long playerId) {
-        LOGGER.info("Calling deletePlayer method from controller.");
+        LOGGER.info("Calling deletePlayer method from player controller.");
         return playerService.deletePlayer(playerId);
     }
 
     // http://localhost:9092/api/player/{playerId}/draw
     @GetMapping(path="/player/{playerId}/draw")
     public String drawUpToTen(@PathVariable Long playerId) {
-        LOGGER.info("Calling drawUpToTen method from controller.");
+        LOGGER.info("Calling drawUpToTen method from player controller.");
         return playerService.drawUpToTen(playerId);
     }
-
-    // http://localhost:9092/api/player/{playerId}/play
-    @GetMapping(path="/player/{playerId}/play")
-    public void playGame(@PathVariable Long playerId) {
-        LOGGER.info("Calling drawUpToTen method from controller.");
-        playerService.playGame(playerId);
+    
+    // http://localhost:9092/api/player/{playerIds}/play
+    @PostMapping(path="/player/play")
+    public void playGame(@RequestBody HashMap<String, ArrayList<Long>> players) {
+        LOGGER.info("Calling playGame method from player controller.");
+        playerService.playGame(players);
     }
 }
