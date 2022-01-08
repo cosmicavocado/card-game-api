d5bdc83486d9725014a4be54bfb452b59df7613c
Mariah Legg <mariah.legg@gmail.com>
2022-01-07 23:07:24 -0500

    Method playGame working as expected.

--- src/main/java/com/game/gameapp/service/GameService.java
+++ src/main/java/com/game/gameapp/service/GameService.java
@@ -4,7 +4,6 @@ import com.game.gameapp.model.Card;
 import com.game.gameapp.model.Player;
 import com.game.gameapp.model.Prompt;
 import com.game.gameapp.repository.CardRepository;
-import com.game.gameapp.repository.GameRepository;
 import com.game.gameapp.repository.PlayerRepository;
 import com.game.gameapp.repository.PromptRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,10 +38,9 @@ public class GameService {
     }
 
     public void drawUpToTen(Long playerId) {
-        LOGGER.info("Calling drawUpToTen method from game service.");
+//        LOGGER.info("Calling drawUpToTen method from game service.");
         Optional<Player> player = playerRepository.findById(playerId);
-        if (player.get().getHand().size()<10) {
-            LOGGER.info("Player " + player.get().getName() + " is drawing up to 10 cards");
+        if (player.isPresent() && player.get().getHand().size()<10) {
             do {
                 int n = rng.nextInt(deck.size());
                 Card newCard = deck.get(n);
@@ -64,9 +62,10 @@ public class GameService {
         // For all playerIds
         for (Long playerId : playerIds) {
             Optional<Player> player = playerRepository.findById(playerId);
+            // Check that player exists
             if (player.isPresent()) {
                 currentPlayers.add(player.get());
-                LOGGER.info("Player " + player.get().getName() + " w/ id " + player.get().getName() + " added to the game.");
+                LOGGER.info("Player " + player.get().getName() + " w/ id " + player.get().getId() + " added to the game.");
                 // set hand to empty
                 player.get().setHand(new ArrayList<>());
                 // set initial score to 0
@@ -78,52 +77,49 @@ public class GameService {
         prompts = (ArrayList<Prompt>) promptRepository.findAll();
 
         // use rng to pick random player for judge
-        int index = rng.nextInt(players.values().size());
-        Optional<Player> judge = playerRepository.findById(playerIds.get(index));
-        LOGGER.info("The first judge is " + judge.get().getName());
+        int index = rng.nextInt(currentPlayers.size());
+        Player judge = currentPlayers.get(index);
+        LOGGER.info("The first judge is " + judge.getName());
         int topScore = 0;
+        int round = 1;
 
         // while topScore != 10, loop game
-        while (topScore != 10) {
-            // deal cards
-
-
-            // judge draws a prompt
-            int rand = rng.nextInt(prompts.size());
-            Prompt p = prompts.get(rand);
-            prompts.remove(rand);
-
+        while(topScore != 10) {
+            // will change if time***
             ArrayList<Card> responses = new ArrayList<>();
             ArrayList<Player> responsePlayer = new ArrayList<>();
-            // all non judge players give response
-            //TODO FIX TO EXCLUDE JUDGE
-            for(int i=0; i<playerIds.size(); i++) {
-                drawUpToTen(playerIds.get(i));
-                // get non judge player
-                Optional<Player> currPlayer = playerRepository.findById(playerIds.get(i));
-                if (i != index) {
-                    // random response card to simulate player choice
-                    Card randomCard = currPlayer.get().hand.get(rng.nextInt(10));
+
+            // loop all players for this round
+            for(Player player : currentPlayers) {
+                drawUpToTen(player.getId()); // keeps all players at max hand size
+                // if player is judge this round
+                if(player.equals(judge)) {
+                    int n = rng.nextInt(prompts.size());
+                    Prompt prompt = prompts.get(n);
+                    prompts.remove(n);
+                    LOGGER.info(prompt.getText() + " pulled by judge " + judge.getName());
+                } else {
+                    Card randomCard = player.hand.get(rng.nextInt(10));
                     responses.add(randomCard);
-                    responsePlayer.add(currPlayer.get());
-                    LOGGER.info(currPlayer.get().getName() + " played " + randomCard.getText());
+                    responsePlayer.add(player);
+                    LOGGER.info(player.getName() + " played " + randomCard.getText());
                 }
             }
 
             // judge picks winning response (random to simulate gameplay)
-            int resp = rng.nextInt(responses.size());
-            responses.get(resp);
-            Player winner = responsePlayer.get(resp);
+            int n = rng.nextInt(responses.size());
+            responses.get(n);
+            Player winner = responsePlayer.get(n);
 
             // winning player score +1
             winner.setScore(winner.getScore()+1);
-            LOGGER.info(winner.getName()+" new score is " + winner.getScore());
+            LOGGER.info(winner.getName()+" wins! Their new score is " + winner.getScore());
             // if player score > topScore
             if (winner.getScore() > topScore) {
                 // topScore = player score
                 topScore = winner.getScore();
             }
-            // Check for gameover
+            // If game over != true
             if (topScore != 10) {
                 // Rotate next judge
                 int nextJudge = index+1;
@@ -131,11 +127,13 @@ public class GameService {
                 if (nextJudge == playerIds.size()) {
                     nextJudge = 0;
                 }
-                judge = playerRepository.findById(playerIds.get(nextJudge));
-                LOGGER.info("Next judge is "+ judge.get().getName());
+                judge = playerRepository.getById(playerIds.get(nextJudge));
+                LOGGER.info("Next judge is "+ judge.getName());
             } else {
                 LOGGER.info("Game Over! "+ winner.getName() + " wins!!");
             }
+            LOGGER.info("End of round "+ round + ".\n");
+            round++;
         }
     }
 }
